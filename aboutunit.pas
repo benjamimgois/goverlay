84a06e43f8b39630500d2194c4b04529662d02d1
benjamimgois <benjamim.gois@gmail.com>
2020-04-04 18:40:20 -0300

    0.3
    * Added vkBasalt option
    * Implemented vkBasalt-CAS function
    * Implemented Global enable for vkBasalt
    * Revemped interface to make room for vkBasalt
    * Added Donation link on the about page
    * Another fix to $HOME variable implementation

--- aboutunit.pas
+++ aboutunit.pas
@@ -13,22 +13,29 @@ type
 
   TaboutForm = class(TForm)
     Image1: TImage;
+    donateImage: TImage;
+    schoorselinkLabel: TLabel;
+    schoorseMemo: TMemo;
     Memo3: TMemo;
+    mangoMemo: TMemo;
     twitterlink: TImage;
     linkedinlink: TImage;
     linux4elink: TLabel;
     mangolink: TLabel;
-    Memo: TMemo;
-    Memo1: TMemo;
+    descriptionMemo: TMemo;
+    jasonMemo: TMemo;
     Memo2: TMemo;
     PageControl1: TPageControl;
-    StaticText1: TStaticText;
+    GoverlayText: TStaticText;
     TabSheet1: TTabSheet;
     TabSheet2: TTabSheet;
     procedure FormCreate(Sender: TObject);
+    procedure donateImageClick(Sender: TObject);
     procedure Label1Click(Sender: TObject);
     procedure linkedinlinkClick(Sender: TObject);
+    procedure mangolink1Click(Sender: TObject);
     procedure mangolinkClick(Sender: TObject);
+    procedure schoorselinkLabelClick(Sender: TObject);
     procedure twitterlinkClick(Sender: TObject);
   private
 
@@ -60,7 +67,7 @@ begin
     v.FindDefaultBrowser(BrowserPath,BrowserParams);
     debugln(['Path=',BrowserPath,' Params=',BrowserParams]);
 
-    URL:='https://flightlessmango.com/';
+    URL:='https://github.com/flightlessmango/MangoHud';
     p:=System.Pos('%s', BrowserParams);
     System.Delete(BrowserParams,p,2);
     System.Insert(URL,BrowserParams,p);
@@ -76,6 +83,42 @@ begin
   finally
     v.Free;
   end;
+end;
+
+procedure TaboutForm.schoorselinkLabelClick(Sender: TObject);
+
+
+
+    var
+  v: THTMLBrowserHelpViewer;
+  BrowserPath, BrowserParams: string;
+  p: LongInt;
+  URL: String;
+  BrowserProcess: TProcessUTF8;
+
+begin
+  v:=THTMLBrowserHelpViewer.Create(nil);
+  try
+    v.FindDefaultBrowser(BrowserPath,BrowserParams);
+    debugln(['Path=',BrowserPath,' Params=',BrowserParams]);
+
+    URL:='https://github.com/DadSchoorse/vkBasalt';
+    p:=System.Pos('%s', BrowserParams);
+    System.Delete(BrowserParams,p,2);
+    System.Insert(URL,BrowserParams,p);
+
+    // start browser
+    BrowserProcess:=TProcessUTF8.Create(nil);
+    try
+      BrowserProcess.CommandLine:=BrowserPath+' '+BrowserParams;
+      BrowserProcess.Execute;
+    finally
+      BrowserProcess.Free;
+    end;
+  finally
+    v.Free;
+  end;
+
 end;
 
 procedure TaboutForm.twitterlinkClick(Sender: TObject);
@@ -142,6 +185,11 @@ procedure TaboutForm.linkedinlinkClick(Sender: TObject);
    end;
  end;
 
+procedure TaboutForm.mangolink1Click(Sender: TObject);
+begin
+
+end;
+
 procedure TaboutForm.Label1Click(Sender: TObject);
 
 
@@ -180,6 +228,38 @@ begin
   //Centralize window
   Left:=(Screen.Width-Width)  div 2;
   Top:=(Screen.Height-Height) div 2;
+end;
+
+procedure TaboutForm.donateImageClick(Sender: TObject);
+
+   var
+  v: THTMLBrowserHelpViewer;
+  BrowserPath, BrowserParams: string;
+  p: LongInt;
+  URL: String;
+  BrowserProcess: TProcessUTF8;
+begin
+  v:=THTMLBrowserHelpViewer.Create(nil);
+  try
+    v.FindDefaultBrowser(BrowserPath,BrowserParams);
+    debugln(['Path=',BrowserPath,' Params=',BrowserParams]);
+
+    URL:='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=Q5EYYEJ5NSJAU&source=url';
+    p:=System.Pos('%s', BrowserParams);
+    System.Delete(BrowserParams,p,2);
+    System.Insert(URL,BrowserParams,p);
+
+    // start browser
+    BrowserProcess:=TProcessUTF8.Create(nil);
+    try
+      BrowserProcess.CommandLine:=BrowserPath+' '+BrowserParams;
+      BrowserProcess.Execute;
+    finally
+      BrowserProcess.Free;
+    end;
+  finally
+    v.Free;
+  end;
 end;
 
 
