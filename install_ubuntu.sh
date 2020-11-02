62d790b4a4cda8aa2451b67c371b0e4e8e29948f
benjamimgois <benjamim.gois@gmail.com>
2020-11-02 01:14:00 -0300

    Script for installation on ubuntu based distros

---
+++ install_ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Author Ryan - Intelligent Gaming - Linux Tutorials & Gameplay
+# Modified by Benjamim Gois at 02/11/20
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install lazarus -y
+sudo apt install git -y
+git clone --recurse-submodules https://github.com/flightlessmango/MangoHud.git cd MangoHud
+./build.sh build
+./build.sh package
+./build.sh install
+cd .. rm -rf MangoHud
+git clone https://github.com/DadSchoorse/vkBasalt.git
+cd vkBasalt
+meson --buildtype=release --prefix=/usr builddir
+ninja -C builddir install
+cd .. rm -rf vkBasalt
+git clone https://github.com/benjamimgois/goverlay.git
+cd goverlay lazbuild -B goverlay.lpi
+sudo mv goverlay /usr/games/
+cd .. rm -rf goverlay
