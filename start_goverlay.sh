4f1093a1b8f8d314c5b4ca0aafa802b6c25ba99e
Benjamim Gois <benjamim.gois@gmail.com>
2021-10-18 14:36:13 -0300

    Force the application to run in x11 mode

--- start_goverlay.sh
+++ start_goverlay.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-# Created by Benjamim Gois at 10/09/21
-# Launch Goverlay
+# This script will Launch Goverlay the correct way
+#
+# QT_QPA_PLATFORM=xcb will force the application to run in x11 mode, so it works on wayland desktops.
+# mangohud --dlsym will force the mangohud display on the spinning cube on goverlay
+# --style fusion will make sure the interface doesn't break in diferent DE and QT themes.
 
-mangohud --dlsym ./goverlay --style fusion
+QT_QPA_PLATFORM=xcb mangohud --dlsym goverlay --style fusion
