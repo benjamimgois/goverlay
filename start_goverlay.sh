caed4754166902f857a21d33d469a02afda6f2e6
Benjamim Gois <benjamim.gois@gmail.com>
2021-12-15 22:16:54 -0300

    switch from fusion to breeze

--- start_goverlay.sh
+++ start_goverlay.sh
@@ -3,6 +3,6 @@
 #
 # QT_QPA_PLATFORM=xcb will force the application to run in x11 mode, so it works on wayland desktops.
 # mangohud --dlsym will force the mangohud display on the spinning cube on goverlay
-# --style fusion will make sure the interface doesn't break in diferent DE and QT themes.
+# --style breeze will make sure the interface doesn't break in diferent DE and QT themes.
 
-QT_QPA_PLATFORM=xcb mangohud --dlsym ./goverlay --style fusion
+QT_QPA_PLATFORM=xcb mangohud --dlsym ./goverlay --style breeze
