57183ce7c94470b844e4ea01a09e72196dcfb18b
Stephan Lachnit <stephanlachnit@debian.org>
2021-12-16 11:28:39 +0100

    Remove start_script.sh again
    Create by either 'make' or 'make start_goverlay.sh'
    
    Signed-off-by: Stephan Lachnit <stephanlachnit@debian.org>

--- start_goverlay.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This script will Launch Goverlay the correct way
-#
-# QT_QPA_PLATFORM=xcb will force the application to run in x11 mode, so it works on wayland desktops.
-# mangohud --dlsym will force the mangohud display on the spinning cube on goverlay
-# --style breeze will make sure the interface doesn't break in diferent DE and QT themes.
-
-QT_QPA_PLATFORM=xcb mangohud --dlsym ./goverlay --style breeze
