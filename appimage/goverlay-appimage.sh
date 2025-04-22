b060343967ada20710e9de7c3a904570b15282a7
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-04-22 13:30:34 -0400

    high `-B` isn't needed anymore

--- appimage/goverlay-appimage.sh
+++ appimage/goverlay-appimage.sh
@@ -64,7 +64,7 @@ echo "Generating AppImage..."
 ./uruntime --appimage-mkdwarfs -f \
 	--set-owner 0 --set-group 0 \
 	--no-history --no-create-timestamp \
-	--compression zstd:level=22 -S26 -B32 \
+	--compression zstd:level=22 -S26 -B8 \
 	--header uruntime \
 	-i ./AppDir -o GOverlay-"$VERSION"-anylinux-"$ARCH".AppImage
 
