83304c4e49448224023975790b1ff508db812722
Benjamim Gois <benjamim.gois@gmail.com>
2026-05-06 15:48:48 -0300

    chore(release): bump version to 1.8.0 stable

--- overlayunit.pas
+++ overlayunit.pas
@@ -4632,7 +4632,7 @@ begin
 
   //Program Version
   GVERSION := '1.8.0';
-  GCHANNEL := 'git'; //stable ou git
+  GCHANNEL := 'stable'; //stable ou git
 
   // Initialize fgmod directory with embedded scripts
   // This ensures fgmod scripts are always available without downloading
