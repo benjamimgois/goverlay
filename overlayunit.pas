d05077a481a4556a8bda7cf7f0da8f5735c792b5
Benjamim Gois <benjamim.gois@gmail.com>
2026-07-06 18:36:41 -0300

    chore: set GCHANNEL to stable and update IDE configurations

--- overlayunit.pas
+++ overlayunit.pas
@@ -2666,7 +2666,7 @@ begin
 
   //Program Version
   GVERSION := '1.8.6';
-  GCHANNEL := 'git'; //stable ou git
+  GCHANNEL := 'stable'; //stable ou git
 
   // Initialize bgmod directory with embedded scripts
   // This ensures bgmod scripts are always available without downloading
