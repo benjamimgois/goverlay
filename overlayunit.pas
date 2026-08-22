dcafa7ab51106b0119fe9f15f2db19ab8d1c4f5b
Benjamim Gois <benjamim.gois@gmail.com>
2026-08-22 16:49:13 -0300

    chore: set release channel to stable for 1.9.0

--- overlayunit.pas
+++ overlayunit.pas
@@ -3256,7 +3256,7 @@ begin
 
   //Program Version
   GVERSION := '1.9.0';
-  GCHANNEL := 'git'; //stable ou git
+  GCHANNEL := 'stable'; //stable ou git
 
   // Initialize bgmod directory with embedded scripts
   // This ensures bgmod scripts are always available without downloading
